d6528dcdc02f8b35a0794123313988c7b221a42e
heronred <heronred@gmail.com>
2026-05-01 16:18:50 -0300

    fix: basename router

--- src/App.tsx
+++ src/App.tsx
@@ -90,7 +90,7 @@ function AppRoutes() {
 export default function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
+        <BrowserRouter basename="/ranking-tm">
         <AppRoutes />
       </BrowserRouter>
     </AuthProvider>
